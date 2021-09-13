1969b6fae822e0bdfd732c85776b48c1afa95890
Reginaldo <reginaldo007oliveira@gmail.com>
2021-09-13 10:14:46 -0300

    NotaFixada

---
+++ notaFixada.js
@@ -0,0 +1,10 @@
+var nome = "Na Vibe"
+console.log(nome)
+var numUm = 3
+var numDois = 5
+var numTres = 9 
+var numQuatro = 6
+
+var final = (numUm + numDois + numTres + numQuatro)/4
+var notaFixada = final.toFixed(1)
+console.log(notaFixada)
